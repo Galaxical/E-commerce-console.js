9ad69337185deedfa33da935f2952cf96e19aa35
Galaxical <azisomtochukwu@gmail.com>
2024-01-18 01:19:30 +0100

    rough work

---
+++ commerceEconsole.js
@@ -0,0 +1,8 @@
+//set up product cart: id, name, price, quantity
+
+const products = [
+    {id: 1, name: "T-shirt", price: 25, quantity: 5},
+    {id: 2, name: "airJordan", price: 150, quantity: 2},
+    {id: 3, name: "sweadPants", price: 65, quantity: 3},
+    {id: 4, name: "baseBall Cap", price: 5, quantity: 2}
+];
